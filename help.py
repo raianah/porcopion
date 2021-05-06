b9f8509f750cff632512cfa5145cd5246b15046c
raianah <82108512+raianah@users.noreply.github.com>
2021-05-06 20:42:51 +0800

    its just help

--- help.py
+++ help.py
@@ -9,10 +9,15 @@ client.remove_command('help')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Readthedocs | !help p<num>."))
-    print('This code is for documentation only.')
-
-
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Documentation | !help p<num>."))
+    print('This repository is for documentation only.')
+    
+@client.command()
+async def hello(ctx):
+    embed = discord.Embed(timestamp=datetime.datetime.utcnow(), color=discord.Color.dark_blue())
+    embed.add_field(name='\u200b', value=f'Hello there, {ctx.author.mention}!)
+    embed.set_footer(text='Giving you a warm cup of coffee to start up your day!')
+    await ctx.send(embed=embed)
 
 @client.group(invoke_without_command=True)
 async def help(ctx):
@@ -22,41 +27,17 @@ async def help(ctx):
         timestamp = datetime.datetime.utcnow(),
         color = 0x855988
     )
-    em.add_field(name='\u200b', value='👉 | **Page 1 (P1)**: Latest updates to the bot.', inline=False)
-    em.add_field(name='\u200b', value='👉 | **Page 2 (P2)**: Entertainment commands.', inline=False)
-    em.add_field(name='\u200b', value='👉 | **Page 3 (P3)**: Mini-game commands.', inline=False)
-    em.add_field(name='\u200b', value='👉 | **Page 4 (P4)**: Suggestion commands.', inline=False)
-    em.add_field(name='\u200b', value='👉 | **Page 5 (P5)**: Invite links.', inline=False)
-    em.add_field(name='\u200b', value='👉 | **Page 6 (P6)**: Moderation commands (soon).', inline=False)
-    em.add_field(name='\u200b', value='👉 | **Page 7 (P7)**: Suggestion results', inline=False)
-    em.add_field(name='\u200b', value='👉 | **Page 8 (total)**: Total commands available for this patch.', inline=False)
+    em.add_field(name='\u200b', value='👉 | **Page 1 (P1)**: Entertainment commands.', inline=False)
+    em.add_field(name='\u200b', value='👉 | **Page 2 (P2)**: Mini-game commands.', inline=False)
+    em.add_field(name='\u200b', value='👉 | **Page 3 (P3)**: Suggestion commands.', inline=False)
+    em.add_field(name='\u200b', value='👉 | **Page 4 (P4)**: Moderation commands.', inline=False)
     em.set_footer(text=f'• Twilight • {ctx.author.name} • >help p1 for next page', icon_url="https://cdn.discordapp.com/attachments/831412988111487017/836129293070499900/20210426_140531_0000.png")
     await ctx.send(embed=em)
 
 @help.command(aliases=['P1'])
 async def p1(ctx):
     em = discord.Embed(
-        title='💾 | Page 1/8 (PATCH NOTES 0.1.0)',
-        description='Here is the current patch, and some noticeable updates to the bot.',
-        timestamp = datetime.datetime.utcnow(),
-        color = 0x6B4984
-    )
-    em.set_thumbnail(url='https://cdn.discordapp.com/attachments/831412988111487017/836150693428592640/W_Y_R_2___9.png')
-    em.add_field(name='#1', value='Added `permamute`, `unmute`, `mute`, `kick` & `ban` commands in preparation for moderator opening.', inline=False)
-    em.add_field(name='#2', value='Added AFK command which acts like mute command but for yourself only.', inline=False)
-    em.add_field(name='#3', value='Added 40+ numbers in `pickanum` command.', inline=False)
-    em.add_field(name='#4', value='Added profanity filter.', inline=False)
-    em.add_field(name='#5', value="Added 10 new topics, 10 new wyr's, 10 new riddles, 10 new tmaj's, and 10 new answers from both `>ask` & `>ask2`.", inline=False)
-    em.add_field(name='#6', value='Added aliases for lesser inputs. Please be guided by using the `help P(num)` command.', inline=False)
-    em.add_field(name='#7', value='Truth or Dare command has been removed.', inline=False)
-    em.add_field(name='#8', value='Added `quiz` command with `15` questions each with multiple choices. You decide which is right or wrong.', inline=False)
-    em.set_footer(text=f'• Twilight • {ctx.author.name} • >help p2 for next page ', icon_url="https://cdn.discordapp.com/attachments/831412988111487017/836129293070499900/20210426_140531_0000.png")
-    await ctx.send(embed=em)
-
-@help.command(aliases=['P2'])
-async def p2(ctx):
-    em = discord.Embed(
-        title = '🎉 | Page 2/8 (ENTERTAINMENT COMMANDS)',
+        title = '🎉 | Page 1/4 (ENTERTAINMENT COMMANDS)',
         description = 'Here is the lists of available entertainment commands.',
         timestamp = datetime.datetime.utcnow(),
         color = 0x483475
@@ -72,10 +53,10 @@ async def p2(ctx):
     em.set_footer(text=f'• Twilight • {ctx.author.name} • >help p3 for next page ', icon_url="https://cdn.discordapp.com/attachments/831412988111487017/836129293070499900/20210426_140531_0000.png")
     await ctx.send(embed=em)
 
-@help.command(aliases=['P3'])
-async def p3(ctx):
+@help.command(aliases=['P2'])
+async def p2(ctx):
     em = discord.Embed(
-        title = '🎮 | Page 3/8 (MINI-GAME COMMANDS)',
+        title = '🎮 | Page 2/4 (MINI-GAME COMMANDS)',
         description = 'Here is the lists of available mini-game commands.',
         timestamp = datetime.datetime.utcnow(),
         color = 0x2B2F77
@@ -87,10 +68,10 @@ async def p3(ctx):
     em.set_footer(text=f'• Twilight • {ctx.author.name} • >help p4 for next page ', icon_url="https://cdn.discordapp.com/attachments/831412988111487017/836129293070499900/20210426_140531_0000.png")
     await ctx.send(embed=em)
 
-@help.command(aliases=['P4'])
-async def p4(ctx):
+@help.command(aliases=['P3'])
+async def p3(ctx):
     em = discord.Embed(
-        title = '💡 | Page 4/8 (SUGGESTION COMMANDS)',
+        title = '💡 | Page 3/4 (SUGGESTION COMMANDS)',
         descriptipn = 'Here is the lists of available suggestion commands.',
         timestamp = datetime.datetime.utcnow(),
         color = 0x141852
@@ -105,23 +86,10 @@ async def p4(ctx):
     em.set_footer(text=f'• Twilight • {ctx.author.name} • >help p5 for next page ', icon_url="https://cdn.discordapp.com/attachments/831412988111487017/836129293070499900/20210426_140531_0000.png")
     await ctx.send(embed=em)
 
-@help.command(aliases=['P5'])
-async def p5(ctx):
+@help.command(aliases=['P4'])
+async def p4(ctx):
     em = discord.Embed(
-        title = '✉️ | PAGE 5/8 ~~(USELESS PAGE)~~ (INVITE LINKS)',
-        description = 'Support Jukebox, Vinyl, and Twilight! Vote them.',
-        timestamp = datetime.datetime.utcnow(),
-        color = 0x070B34
-    )
-    em.add_field(name='Jukebox', value='Join Jukebox in your server [here](https://top.gg/bot/829596382977327146).', inline=False)
-    em.add_field(name='Twilight', value='Soon.', inline=False)
-    em.set_footer(text=f'• Twilight • {ctx.author.name} • >help p6 for next page ', icon_url="https://cdn.discordapp.com/attachments/831412988111487017/836129293070499900/20210426_140531_0000.png")
-    await ctx.send(embed=em)
-
-@help.command(aliases=['P6'])
-async def p6(ctx):
-    em = discord.Embed(
-        title = '👮 | PAGE 6/8 (MODERATION & UTILITY COMMANDS)',
+        title = '👮 | PAGE 4/4 (MODERATION & UTILITY COMMANDS)',
         description = 'Here is the lists of available utility commands.',
         timestamp = datetime.datetime.utcnow(),
         color = 0x375597
@@ -134,36 +102,5 @@ async def p6(ctx):
     em.add_field(name='Unmute *(umt)*', value='Un-mute members earlier than the given time.', inline=False)
     em.set_footer(text=f'• Twilight • {ctx.author.name} • >help p7 for next page ', icon_url="https://cdn.discordapp.com/attachments/831412988111487017/836129293070499900/20210426_140531_0000.png")
     await ctx.send(embed=em)
-
-@help.command(aliases=['P7'])
-async def p7(ctx):
-    em = discord.Embed(
-        title = '📩 | PAGE 7/8 (SUGGESTION RESULTS)',
-        description = 'See all reviewed suggestions here.',
-        timestamp = datetime.datetime.utcnow(),
-        color = 0xfd5e53
-    )
-    em.set_footer(text=f'• Twilight • {ctx.author.name} • >help total for next page ', icon_url="https://cdn.discordapp.com/attachments/831412988111487017/836129293070499900/20210426_140531_0000.png")
-    em.add_field(name='\u200b', value='┏━━━━━━━━━━༻❁༺━━━━━━━━━━┓', inline=False)
-    em.add_field(name='Approved Suggestions:', value='**0**', inline=True)
-    em.add_field(name='Declined Suggestions:', value='**0**', inline=True)
-    em.add_field(name='┗━━━━━━━━━━༻❁༺━━━━━━━━━━┛', value='\u200b', inline=False)
-    em.add_field(name='\u200b', value='\u200b')
-    em.add_field(name='\u200b', value='┏━━━━━━━━━━༻❁༺━━━━━━━━━━┓', inline=False)
-    em.add_field(name='#1', value='N/A.', inline=False)
-    em.add_field(name='#2', value='N/A.', inline=False)
-    em.add_field(name='#3', value='N/A.', inline=False)
-    em.add_field(name='┗━━━━━━━━━━༻❁༺━━━━━━━━━━┛', value='\u200b', inline=False)
-    await ctx.send(embed=em)
-
-@help.command()
-async def total(ctx):
-    em = discord.Embed(
-        title = '👉 | PAGE 8/8 (TOTAL COMMANDS)',
-        description = 'This page is nothing but total commands. Seriously.',
-        timestamp = datetime.datetime.utcnow(),
-        color = 0xc57893
-    )
-    em.add_field(name='\u200b', value='**Total Commands:** *35!*')
-    em.set_footer(text=f'• Twilight • {ctx.author.name} • this is the last page ', icon_url='')
-    await ctx.send(embed=em)
+    
+@client.token('token')
